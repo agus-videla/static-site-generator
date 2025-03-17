36f840664fcbf1ed5603203932d73374ea181b0f
agus-videla <agustinvidela0198@gmail.com>
2025-03-17 09:27:30 -0300

    blocktype enum

--- src/block_utils.py
+++ src/block_utils.py
@@ -1,3 +1,6 @@
+import re
+from markdown_blocks import BlockType
+
 def markdown_to_blocks(markdown: str):
     result = []
     for block in markdown.split('\n\n'):
@@ -5,3 +8,15 @@ def markdown_to_blocks(markdown: str):
         filtered = filter(lambda x: x != '', stripped_lines)
         result.append('\n'.join(filtered))
     return result
+
+def block_to_block_type(markdown: str):
+    if(re.match(r"^#{1,7}\s", markdown)):
+        return BlockType.HEADING
+    if(re.match(r"^```[\s\S]+```$", markdown)):
+        return BlockType.CODE
+    if(re.match(r"", markdown)):
+        return BlockType.QUOTE
+    if(re.match(r"^```.+```$", markdown)):
+        return BlockType.ORDERED_LIST
+    if(re.match(r"^>$", markdown)):
+        return BlockType.CODE
